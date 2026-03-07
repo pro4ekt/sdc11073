83183ed0a14af39ae80aef7b24368b0c64d16980
pro4ekt <zhakaprobro@gmail.com>
2026-03-07 15:14:28 +0100

    Уточнение и тесты

--- MyTests/Wave/provider_wave.py
+++ MyTests/Wave/provider_wave.py
@@ -24,7 +24,7 @@ if __name__ == '__main__':
     my_uuid = uuid.uuid5(base_uuid, "12345")
 
     # Подгрузка mdib с файла
-    mdib = ProviderMdib.from_mdib_file("mdib.xml")
+    mdib = ProviderMdib.from_mdib_file("../Stuff/mdib.xml")
 
     # Объявление компонентов(полей) провайдера
     model = ThisModelType(model_name='TestModel',
