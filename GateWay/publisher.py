08fb2008e4c7c8d5788d8dfb1e89c8d225f9a267
pro4ekt <zhekaprobro@gmail.com>
2026-03-31 17:39:37 +0200

    Добавил простейшего publisher

---
+++ GateWay/publisher.py
@@ -0,0 +1,88 @@
+"""
+Example creating a publisher that sends an Int32, String, Bool and ArrayInt16 from AddressSpace
+"""
+
+import asyncio
+import logging
+
+from asyncua import Server, ua, pubsub
+from asyncua.common.node import Node
+from asyncua.ua.uatypes import NodeId, VariantType
+
+URL = "opc.udp://10.188.154.140:4840"
+PDSNAME = "SimpleDataSet"
+
+
+async def create_published_dataset(server: Server, nodes: list[Node]) -> pubsub.PublishedDataItems:
+    # Links nodes from AddressSpace to PubSub (To simplfy use displayname as fieldname)
+    variables = []
+    for node in nodes:
+        name = await node.read_display_name()
+        variables.append(pubsub.TargetVariable(name.Text, node.nodeid))
+    return await pubsub.PublishedDataItems.Create(PDSNAME, server, variables)
+
+
+async def create_variables(node: Node, ns: ua.UInt16) -> list[Node]:
+    nodes = []
+    folder = await node.add_folder(NodeId("PublisherDemo", ns), "PublisherDemoNodes")
+    nodes.append(await folder.add_variable(NodeId("PubInt32", ns), "Int32", 1, VariantType.Int32))
+    nodes.append(await folder.add_variable(NodeId("PubString", ns), "String", "DemoString"))
+    nodes.append(await folder.add_variable(NodeId("PubBool", ns), "Bool", True, VariantType.Boolean))
+    nodes.append(await folder.add_variable(NodeId("PubArrayInt16", ns), "ArrayInt16", [1, 2, 3], VariantType.Int16))
+    for n in nodes:
+        await n.set_writable()
+    return nodes
+
+
+async def main():
+    logging.basicConfig(level=logging.INFO)
+    # setup our server
+    server = Server()
+    await server.init()
+    server.set_endpoint("opc.tcp://10.188.154.140:4840/freeopcua/server/")
+
+    # setup our own namespace, not really necessary but should as spec
+    uri = "http://examples.freeopcua.github.io"
+    idx = await server.register_namespace(uri)
+    nodes = await create_variables(server.nodes.objects, idx)
+    ps = await server.get_pubsub()
+    async with server:
+        pds = await create_published_dataset(server, nodes)
+        con = pubsub.PubSubConnection.udp_uadp(
+            "Publisher Connection1 UDP UADP",
+            ua.UInt16(1),
+            pubsub.UdpSettings(Url=URL),
+            writer_groups=[
+                # Configures the writer the publish every 5000
+                pubsub.WriterGroup.new_uadp(
+                    name="WriterGroup1",
+                    writer_group_id=ua.UInt16(1),
+                    publishing_interval=5000,
+                    writer=[
+                        pubsub.DataSetWriter.new_uadp(
+                            name="Writer1",
+                            dataset_writer_id=ua.UInt16(32),
+                            dataset_name=PDSNAME,
+                            datavalue=True,
+                        )
+                    ],
+                )
+            ],
+        )
+        await ps.add_connection(con)
+        await ps.add_published_dataset(pds)
+        await ps.start()
+
+        count = 0
+        while 1:
+            await asyncio.sleep(2)
+            count += 1
+            await nodes[0].write_value(count, ua.VariantType.Int32)
+            await nodes[1].write_value(f"DemoString {count}", ua.VariantType.String)
+            await nodes[2].write_value(count % 2 == 0, ua.VariantType.Boolean)
+            await nodes[3].write_value([count, count + 1, count + 2], ua.VariantType.Int16)
+            print(f"Publisher updated nodes: Int32={count}, String='DemoString {count}', Bool={count % 2 == 0}, ArrayInt16={[count, count + 1, count + 2]}")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
